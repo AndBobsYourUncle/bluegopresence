814fdc26bff31b9bc6d2c2e565e66565fbe80694
Nicholas Page <nicholas.page@goat.com>
2021-12-12 14:21:21 -0800

    add yaml parsing

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module bluegopresence
 
 go 1.15
 
-require tinygo.org/x/bluetooth v0.4.0
+require (
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	tinygo.org/x/bluetooth v0.4.0
+)
--- main.go
+++ main.go
@@ -1,17 +1,46 @@
 package main
 
-import "tinygo.org/x/bluetooth"
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+	"tinygo.org/x/bluetooth"
+)
 
 var adapter = bluetooth.DefaultAdapter
 
+type KnownDevice struct {
+	MacAddress string `yaml:"mac_address"`
+}
+
+type KnownStaticDevices struct {
+	Devices []KnownDevice `yaml:"devices"`
+}
+
 func main() {
+	body, err := ioutil.ReadFile("./known_static_devices.yaml")
+	if err != nil {
+		panic("failed to open known_static_devices.yaml")
+	}
+
+	knownStaticDevices := KnownStaticDevices{}
+
+	err = yaml.Unmarshal(body, &knownStaticDevices)
+	if err != nil {
+		panic("error parsing known_static_devices.yaml")
+	}
+
 	// Enable BLE interface.
 	must("enable BLE stack", adapter.Enable())
 
 	// Start scanning.
 	println("scanning...")
-	err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
-		println("found device:", device.Address.String(), device.RSSI, device.LocalName())
+	err = adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
+		for _, knownDevice := range knownStaticDevices.Devices {
+			if knownDevice.MacAddress == device.Address.String() {
+				println("found device:", device.Address.String(), device.RSSI, device.LocalName())
+			}
+		}
 	})
 	must("start scan", err)
 }
