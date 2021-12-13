62b1d2c2fcffc133a50795af90fa17533ceeb79a
Nicholas Page <nicholas.page@goat.com>
2021-12-13 15:57:29 -0800

    publish to mqtt

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module bluegopresence
 go 1.15
 
 require (
+	github.com/eclipse/paho.mqtt.golang v1.3.5 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	tinygo.org/x/bluetooth v0.4.0
 )
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 
 	"gopkg.in/yaml.v2"
 	"tinygo.org/x/bluetooth"
@@ -17,6 +20,14 @@ type KnownStaticDevices struct {
 	Devices []KnownDevice `yaml:"devices"`
 }
 
+type MqttConfig struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Topic    string `json:"topic"`
+}
+
 func main() {
 	body, err := ioutil.ReadFile("./known_static_devices.yaml")
 	if err != nil {
@@ -30,6 +41,28 @@ func main() {
 		panic("error parsing known_static_devices.yaml")
 	}
 
+	body, err = ioutil.ReadFile("./mqtt_config.yaml")
+	if err != nil {
+		panic("failed to open mqtt_config.yaml")
+	}
+
+	mqttConfig := MqttConfig{}
+
+	err = yaml.Unmarshal(body, &mqttConfig)
+	if err != nil {
+		panic("error parsing mqtt_config.yaml")
+	}
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", mqttConfig.Host, mqttConfig.Port))
+	opts.SetClientID("bluegopresence")
+	opts.SetUsername(mqttConfig.Username)
+	opts.SetPassword(mqttConfig.Password)
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
 	// Enable BLE interface.
 	must("enable BLE stack", adapter.Enable())
 
@@ -38,6 +71,10 @@ func main() {
 	err = adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
 		for _, knownDevice := range knownStaticDevices.Devices {
 			if knownDevice.MacAddress == device.Address.String() {
+				text := fmt.Sprintf("%d", device.RSSI)
+				token := client.Publish("bluegopresence/"+mqttConfig.Topic+"/"+knownDevice.MacAddress+"/rssi", 0, false, text)
+				token.Wait()
+
 				println("found device:", device.Address.String(), device.RSSI, device.LocalName())
 			}
 		}
